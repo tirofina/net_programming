32de1f2ef7d9a73b19af5f56fb5ce4095b8eef05
tirofina <44771826+tirofina@users.noreply.github.com>
2022-04-12 22:26:14 +0900

    HW7

---
+++ HW7/hw7_udp_loss_chat_client.py
@@ -0,0 +1,35 @@
+from socket import *
+import random
+import time
+
+port = 3333
+BUFF_SIZE = 1024
+
+sock = socket(AF_INET, SOCK_DGRAM)
+
+while True:
+    msg = input('-> ')
+    reTx = 0
+    while reTx <= 3:
+        resp = str(reTx) + ' ' + msg
+        sock.sendto(resp.encode(), ('localhost', port))
+        sock.settimeout(2)
+
+        try:
+            data, addr = sock.recvfrom(BUFF_SIZE)
+        except timeout:
+            reTx += 1
+            continue
+        else:
+            break
+    sock.settimeout(None)
+    while True:
+        data, addr = sock.recvfrom(BUFF_SIZE)
+        
+        # 고의로 50%의 데이터 손실을 만듦
+        if random.random() <= 0.5:
+            continue
+        else:
+            sock.sendto(b'ack', addr)
+            print('<- ', data.decode())
+            break
